85f1463855e1106a148f22c57cf0259f350bafbc
Orkhan481 <orxan17022002@gmail.com>
2022-11-30 18:48:43 +0400

    Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import CustomBtn from "./components/CustomBtn";
+
+function App() {
+  return (
+    <div className="App">
+      <CustomBtn text="Click" hint="Hint"/>
+    </div>
+  );
+}
+
+export default App;
